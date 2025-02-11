720e98312819c943ef27821bfae561f733cdf7fc
Othayz <thairome.413b@gmail.com>
2025-02-10 22:36:41 -0300

    Adds new handlers file and implements functions to manage students

--- Apis/api.go
+++ Apis/api.go
@@ -1,8 +1,6 @@
 package apis
 
 import (
-	"net/http"
-
 	"github.com/Othayz/API/db"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -39,35 +37,3 @@ func (api *API) Start() error{
 	api.Echo.DELETE("/students/:id", api.deleteStudentsByID)
 }
 
-func (api *API) getStudents(c echo.Context) error {
-	Students, err :=api.DB.GetStudentByID()
-	if err != nil {
-	  return c.String(http.StatusNotFound, "error getting students")
-	}
-	return c.JSON(http.StatusOK, Students)
-  }
-  
-  func (api *API) createStudents(c echo.Context) error {
-	student := db.Student{}
-	if err := c.Bind(&student); err != nil {
-	  return err
-	}
-	if err := api.DB.AddStudent(student); err != nil {
-	  return c.String(http.StatusInternalServerError, "error adding student")
-	}
-  
-	return c.String(http.StatusOK, "create a new student")
-  }
-  
-  func (api *API) getStudentsByID(c echo.Context) error {
-	id := c.Param("id")
-	return c.String(http.StatusOK, id)
-  }
-  func (api *API) updateStudentsByID(c echo.Context) error {
-	id := c.Param("id")
-	return c.String(http.StatusOK, id)
-  }
-  func (api *API) deleteStudentsByID(c echo.Context) error {
-	id := c.Param("id")
-	return c.String(http.StatusOK, id)
-  }
